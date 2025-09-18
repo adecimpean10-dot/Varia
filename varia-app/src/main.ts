b0a21443398fabf008bdc7866a2779545a48f55f
AdeCimpean <ade.cimpean.10@gmail.com>
2025-09-18 16:55:55 +0300

    Create working folder structure

--- varia-app/src/main.ts
+++ varia-app/src/main.ts
@@ -1,7 +1,6 @@
 import { mount } from 'svelte'
-import './app.css'
 import App from './App.svelte'
-import './styles/base.scss'
+import '@/styles/base/_reset.scss';
 
 const app = mount(App, {
   target: document.getElementById('app')!,
